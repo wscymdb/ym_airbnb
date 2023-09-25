be26eec6a945a9831625ea914c451ed3821467ea
“wscymdb” <1724215773@qq.com>
2023-09-25 17:46:06 +0800

    feat(router): react router初始化完毕

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React, { memo } from 'react'
+import { useRoutes } from 'react-router-dom'
+import routes from './router'
 
 const App = memo(() => {
-  return <div>App</div>
+  return (
+    <div className="app">
+      <div className="header"></div>
+      <div className="page">{useRoutes(routes)}</div>
+      <div className="footer"></div>
+    </div>
+  )
 })
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-
 import 'normalize.css'
+import { HashRouter } from 'react-router-dom'
+
+import App from './App'
 import '@/assets/css/index.less'
+import Loading from './views/Loding'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <HashRouter>
+    <Suspense fallback={<Loading />}>
+      <App />
+    </Suspense>
+  </HashRouter>
+  // </React.StrictMode>
 )
