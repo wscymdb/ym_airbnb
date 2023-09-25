0b3784405e9ea047d3bfc54ea1e8d90d10918655
“wscymdb” <1724215773@qq.com>
2023-09-25 17:17:40 +0800

    feat(all): init project

---
+++ craco.config.js
@@ -0,0 +1,19 @@
+// react脚手架没有暴露webpack的配置 需要借助craco插件 npm i @craco/craco -D
+// 因为本项目使用的是react-script5 所以需要这样安装 npm i @craco/craco@alpha -D
+// 注意在package.json中将script 改为craco xxx eg：craco start
+const path = require('path')
+const CracoLessPlugin = require('craco-less')
+
+const resolve = (pathname) => path.resolve(__dirname, pathname)
+
+module.exports = {
+  // 配置less
+  plugins: [{ plugin: CracoLessPlugin }],
+  // 配置webpack
+  webpack: {
+    // 配置别名
+    alias: {
+      '@': resolve('src'),
+    },
+  },
+}
